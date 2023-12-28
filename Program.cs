02f7bfacc04909f785b61043ca5c461f3e38f706
furkantaha04 <112404222+furkantaha04@users.noreply.github.com>
2023-12-28 11:37:16 +0300

    substring
    c# substring ve get set kulanarak tc kimlik no  sansur islemi

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__ders_tekrsrı
+{
+    class personel
+    {
+        private string TCNO;
+
+        public string Tckimlik
+        {
+            get
+            {
+               if(TCNO == null)
+                {
+                    return TCNO + "Hatalı giriş yaptınız: ";
+                }
+                else
+                {
+                    return TCNO.Substring(0, 4) + "*******";
+                }
+
+            }
+            set
+            {
+                bool kontrol = false;
+                if(value.Length == 11)
+                {
+                    for(int i = 0; i< value.Length; i++)
+                    {
+                        bool SayiMi = char.IsNumber(value[i]);
+
+                        if(SayiMi)
+                        {
+                            
+                        }
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+                    
+                    if(kontrol)
+                    {
+                        Console.WriteLine("Lütfen karekter değeri giriniz: ");
+                    }
+                    else
+                    {
+                        TCNO = value;
+                    }
+                   
+                }
+                else
+                {
+                    Console.WriteLine( "girdiğiniz karekter 11 haneli olmak zorundadır !! ");
+                }
+
+            }
+        }
+
+    }
+
+
+
+
+
+
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            personel TC = new personel();
+            TC.Tckimlik = "15433420844";
+            Console.WriteLine(" 11 haneli tc numarası : "+TC.Tckimlik);
+         
+          
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
+
+
